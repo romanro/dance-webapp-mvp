1c39eff13a25b61090ec7befbe7f7a23ebdb139b
LitalHamami <lital.hamami@gmail.com>
2020-06-03 20:56:54 +0300

    student routingn - using:loadChildren

--- packages/client/src/app/student/student-routing.module.ts
+++ packages/client/src/app/student/student-routing.module.ts
@@ -11,8 +11,6 @@ const routes: Routes = [
       { path: 'star', loadChildren: () => import('./stars-page/student-stars.module').then(m => m.StudentStarsModule) },
       { path: 'lab', loadChildren: () => import('../lab/lab.module').then(m => m.LabModule) },
       { path: 'practices', loadChildren: () => import('./practices/parctices.module').then(m => m.PracticesModule) },
-      // { path: 'practices/:id', component: PracticePageComponent },
-
       { path: 'profile', loadChildren: () => import('./profile-pages/student-profile.module').then(m => m.StudentProfileModule) },
       { path: 'challenges', loadChildren: () => import('./challenges/student-challenges.module').then(m => m.StudentChallengesModule) },
       { path: '', redirectTo: 'star', pathMatch: 'full' }
