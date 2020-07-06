341d9fff1ea844ffd545d0f70ffb2a14ef2e985b
Roman Rozanov 10053974 <roman.rozanov@gemalto.com>
2020-07-06 23:06:48 +0300

    video models from api added, lab model added

--- packages/client/src/app/_infra/core/models/video.model.ts
+++ packages/client/src/app/_infra/core/models/video.model.ts
@@ -4,15 +4,52 @@ export interface FigureVideo {
     soundTrackUrl?: string;
 }
 
-export interface View {
-    id: string;
-    direction: ViewDirection;
-    videoUrl: string;
-}
-
 export enum ViewDirection {
     RIGHT = 'right',
     LEFT = 'left',
     FRONT = 'front',
     BACK = 'back'
+}
+
+export interface Video {
+    name: string;
+    path: string;
+    view: View;
+    participatesAmount: ParticipatesAmount;
+    associateWith: AssociateType;
+    type: VideoType;
+}
+
+export enum View {
+    FRONT = 'front',
+    BACK = 'back'
+}
+
+export enum ParticipatesAmount {
+    SOLO = 1,
+    COUPLE = 2
+}
+
+export enum AssociateType {
+    VIDEO = 'video',
+    FIGURE = 'figure'
+}
+
+export enum VideoType {
+    // the following states are used for star only
+    PROMO = 'promo',
+    BASIC_PRINCIPLES = 'basicPrinciples',
+    ADDITIONAL = 'additional',
+
+    // the following states are shared for star and user
+    COMPARABLE = 'comparable'
+}
+
+export class LabStarVideo implements Video {
+    name: string;
+    path: string;
+    view: View;
+    participatesAmount: ParticipatesAmount;
+    associateWith: AssociateType = AssociateType.VIDEO;
+    type: VideoType = VideoType.COMPARABLE;
 }
