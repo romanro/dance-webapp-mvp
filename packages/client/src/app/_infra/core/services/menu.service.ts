07acfe52c80b957253b2ce8bec40b8352ac6ebf6
Roman Rozanov 10053974 <roman.rozanov@gemalto.com>
2020-07-09 15:02:18 +0300

    menu open/close state mooved to behavior subject

--- packages/client/src/app/_infra/core/services/menu.service.ts
+++ packages/client/src/app/_infra/core/services/menu.service.ts
@@ -1,10 +1,23 @@
 import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class MenuService {
 
-constructor() { }
+  private menuOpenStateSource = new BehaviorSubject<boolean>(false);
+  menuOpenState$ = this.menuOpenStateSource.asObservable();
+
+  constructor() { }
+
+  setMenuOpenState(state: boolean) {
+    this.menuOpenStateSource.next(state);
+  }
+
+  toggleMenuOpenState() {
+    const currentState = this.menuOpenStateSource.getValue();
+    this.menuOpenStateSource.next(!currentState);
+  }
 
 }
--- packages/client/src/app/_infra/ui/header/header.component.ts
+++ packages/client/src/app/_infra/ui/header/header.component.ts
@@ -1,8 +1,9 @@
 import { Component, HostListener, Input, OnInit } from '@angular/core';
 import { MenuData, MenuItemFunction } from '@core/models/';
-import { LoginService } from '@core/services/';
+import { AlertService, LoginService, MenuService } from '@core/services/';
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import { AboutDanskillModalComponent } from '@ui/about-danskill-modal/about-danskill-modal.component';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'ui-header',
@@ -12,15 +13,19 @@ export class HeaderComponent implements OnInit {
 
   @Input() menuData: MenuData;
 
-  menuIsOpen = false;
+  menuIsOpen$: Observable<boolean>;
   deferredPrompt: any;
   showAddToHomeButton = false;
 
-  constructor(private loginService: LoginService, private modalService: NgbModal) { }
+  constructor(
+    private loginService: LoginService,
+    private modalService: NgbModal,
+    private alertService: AlertService,
+    private menuService: MenuService,
+  ) { }
 
   @HostListener('window:beforeinstallprompt', ['$event'])
   onbeforeinstallprompt(e) {
-    console.log(e);
     // Prevent Chrome 67 and earlier from automatically showing the prompt
     e.preventDefault();
     // Stash the event so it can be triggered later.
@@ -29,15 +34,15 @@ export class HeaderComponent implements OnInit {
   }
 
   ngOnInit() {
-
+    this.menuIsOpen$ = this.menuService.menuOpenState$;
   }
 
   toggleMenu() {
-    this.menuIsOpen = !this.menuIsOpen;
+    this.menuService.toggleMenuOpenState();
   }
 
   closeMenu() {
-    this.menuIsOpen = false;
+    this.menuService.setMenuOpenState(false);
   }
 
   menuItemFunction(fType: MenuItemFunction) {
@@ -52,19 +57,23 @@ export class HeaderComponent implements OnInit {
   }
 
   addToHomeScreen() {
-    // hide our user interface that shows our A2HS button
-    this.showAddToHomeButton = false;
+
     // Show the prompt
     this.deferredPrompt.prompt();
     // Wait for the user to respond to the prompt
     this.deferredPrompt.userChoice
       .then((choiceResult) => {
         if (choiceResult.outcome === 'accepted') {
-          console.log('User accepted the A2HS prompt');
+          /// User accepted the A2HS prompt
+          // hide our user interface that shows our A2HS button
+          this.showAddToHomeButton = false;
+          this.deferredPrompt = null;
+          this.alertService.success('COMMON.AddToHomeSuccess');
+          this.closeMenu();
         } else {
-          console.log('User dismissed the A2HS prompt');
+          // User dismissed the A2HS prompt
+          this.alertService.error('COMMON.AddToHomeError');
         }
-        this.deferredPrompt = null;
       });
   }
 
