a438495dc35136673c4c5bc41c58d315b5f259ba
Roman Rozanov 10053974 <roman.rozanov@gemalto.com>
2020-07-27 17:45:12 +0300

    nav component fully styled

--- packages/client/src/app/_infra/ui/navigation/navigation.component.ts
+++ packages/client/src/app/_infra/ui/navigation/navigation.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, HostListener, Input, OnInit } from '@angular/core';
 import { NavButton } from '@core/models';
 
 @Component({
@@ -8,10 +8,21 @@ import { NavButton } from '@core/models';
 export class NavigationComponent implements OnInit {
 
   @Input() navButtons: NavButton[];
+  userScrolled = false;
 
   constructor() { }
 
+  @HostListener('window:scroll', ['$event'])
+  onWindowScroll() {
+    this.detectScrollPosition();
+  }
+
+
   ngOnInit() {
+  }
+
+  detectScrollPosition() {
+    this.userScrolled = document.documentElement.scrollTop > 80;
   }
 
 }
