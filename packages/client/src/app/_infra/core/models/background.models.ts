420059b886460c72eb4a5c08dcd844b5a914371c
Roman Rozanov 10053974 <roman.rozanov@gemalto.com>
2020-08-31 23:39:43 +0300

    bg animations added

---
+++ packages/client/src/app/_infra/core/models/background.models.ts
@@ -0,0 +1,17 @@
+export class BackgroundPosition {
+    posX: string;
+    posY: string;
+    size: string;
+
+    constructor() {
+        this.posX = `${this.getRandomInt(0, 150)}%`;
+        this.posY = `${this.getRandomInt(0, 150)}%`;
+        this.size = `${this.getRandomInt(40, 250)}%`;
+    }
+
+    private getRandomInt(min: number, max: number): number {
+        min = Math.ceil(min);
+        max = Math.floor(max);
+        return Math.floor(Math.random() * (max - min + 1)) + min;
+    }
+}
